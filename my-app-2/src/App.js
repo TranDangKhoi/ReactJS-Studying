31b314e3131912407066170536602950359b0e34
TranDangKhoi <88824627+TranDangKhoi@users.noreply.github.com>
2022-07-29 16:33:04 +0700

    Learn about useEffect deps and cleanup function

--- my-app-2/src/App.js
+++ my-app-2/src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import Login from "./components/login/Login";
-import Photo2 from "./components/photo (useEffect practice)/Photo2";
+import Timer from "./components/cleanup (useEffect cleanup)/Timer";
+import User from "./components/username (useEffect)/User";
 function App() {
   return (
     <div>
-      <Login></Login>
+      <Timer></Timer>
     </div>
   );
 }
--- my-app-2/src/components/login/Login.js
+++ my-app-2/src/components/login/Login.js
@@ -1,41 +1,35 @@
 import React, { useEffect, useState } from "react";
 
-const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  useEffect(() => {
-    if(localStorage.getItem("user-info")){
-        history.push("/add")
-    }
-  }, []);
-  function login() {
-    console.warn(email, password);
-    let item = { email, password };
-    let result = await fetch("",{
-        method:"POST",
-        headers:{
-            "Content-Type":"application-json",
-            "Accept":"application/json"
-        },
-        body:JSON.stringify(item);
-    });
-    result = await result.json();
+import axios from "axios";
+
+const GetCustomerLogin = async () => {
+  try {
+    const response = await axios.get("http://localhost:9090/api/v1/customer");
+    console.log(response);
+    return response.data;
+  } catch (error) {
+    console.log(error);
   }
+};
+
+const Login = () => {
+  const [userEmail, setuserEmail] = useState([]);
+  useEffect(() => {
+    GetCustomerLogin().then((infos) => {
+      console.log(infos);
+      setuserEmail(infos);
+    });
+  }, []);
 
   return (
     <div>
-      <h1>Login Testing</h1>
-      <input
-        type="text"
-        placeholder="Enter your email"
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Enter your email"
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={login}>Login</button>
+      <div className="username">
+        {userEmail.map((item, index) => (
+          <div className="user-email" key={item.cusId}>
+            {item.cusEmail}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
