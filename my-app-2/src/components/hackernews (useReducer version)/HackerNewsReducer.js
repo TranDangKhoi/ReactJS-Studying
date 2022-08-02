e191fad2b92ca624d71f13be58d007141b6f018f
TranDangKhoi <88824627+TranDangKhoi@users.noreply.github.com>
2022-08-02 22:48:03 +0700

    clean code useReducer HackerNews

--- my-app-2/src/components/hackernews (useReducer version)/HackerNewsReducer.js
+++ my-app-2/src/components/hackernews (useReducer version)/HackerNewsReducer.js
@@ -1,11 +1,13 @@
-import axios from "axios";
 import React, { useEffect, useReducer, useRef } from "react";
+import axios from "axios";
+//https://hn.algolia.com/api/v1/search?query=react
 
 const initialState = {
   hits: [],
   query: "",
-  loading: true,
-  url: `https://hn.algolia.com/api/v1/search?query=''`,
+  loading: false,
+  errorMsg: "",
+  url: `https://hn.algolia.com/api/v1/search?query='random'`,
 };
 const hackerNewsReducer = (state, action) => {
   switch (action.type) {
@@ -16,6 +18,10 @@ const hackerNewsReducer = (state, action) => {
       return { ...state, loading: action.payload };
     }
 
+    case "SET_ERRORMSG": {
+      return { ...state, errorMsg: action.payload };
+    }
+
     case "SET_QUERY": {
       return { ...state, query: action.payload };
     }
@@ -23,56 +29,49 @@ const hackerNewsReducer = (state, action) => {
     case "SET_URL": {
       return { ...state, url: action.payload };
     }
-
     default:
       break;
   }
 };
-
-const HackerNewsReducer = () => {
+const HackerNews = () => {
   const [state, dispatch] = useReducer(hackerNewsReducer, initialState);
-  //   const [hits, setHits] = useState([]);
-  //   const [query, setQuery] = useState("react");
-  //   const [loading, setLoading] = useState(true);
-  //   const [url, setUrl] = useState(
-  //     `https://hn.algolia.com/api/v1/search?query=${query}`
-  //   );
-  const handleFetchHits = useRef({});
-  handleFetchHits.current = async () => {
+  const handleFetchData = useRef({});
+  handleFetchData.current = async () => {
     dispatch({
       type: "SET_LOADING",
       payload: true,
     });
-    //   setLoading(true);
     try {
       const response = await axios.get(state.url);
+      // setHits(response.data?.hits || []);
       dispatch({
         type: "SET_HITS",
-        payload: response.data?.hits,
+        payload: response.data?.hits || [],
       });
       dispatch({
         type: "SET_LOADING",
         payload: false,
       });
-      // console.log(response);
-      // setHits(response.data?.hits || []);
-      // setLoading(false);
-    } catch (error) {
+    } catch (err) {
       // setLoading(false);
       dispatch({
         type: "SET_LOADING",
         payload: false,
       });
-      // console.log(error);
+      dispatch({
+        type: "SET_ERRORMSG",
+        payload: "Oops! Something went wrong please try again later",
+      });
     }
   };
   useEffect(() => {
-    handleFetchHits.current();
+    handleFetchData.current();
   }, [state.url]);
+  console.log(state.query);
   return (
-    <div className="w-[600px] mx-auto my-5 p-5 bg-white shadow-md rounded-md">
-      <div className="flex gap-x-3">
-        <div className="flex items-center gap-5 w-full border border-gray-200 rounded-lg py-3 px-5">
+    <div className="bg-white w-[600px] mx-auto mt-5 p-7 rounded-lg shadow-md">
+      <div className="flex gap-x-5">
+        <div className="flex items-center gap-5 w-full border border-gray-200 rounded-lg py-3 px-5 focus:border-blue-300">
           <span className="flex-shrink-0 text-gray-500">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -92,8 +91,7 @@ const HackerNewsReducer = () => {
           <input
             type="text"
             className="w-full outline-none bg-transparent"
-            placeholder="Enter your keyword"
-            defaultValue={state.query}
+            placeholder="Enter your content"
             onChange={(e) =>
               dispatch({
                 type: "SET_QUERY",
@@ -103,26 +101,30 @@ const HackerNewsReducer = () => {
           />
         </div>
         <button
-          className="inline-flex items-center justify-center px-8 py-4 font-sans font-semibold tracking-wide text-white bg-blue-500 rounded-lg h-[60px]"
+          className="bg-blue-500 text-white rounded-md p-5"
           onClick={() =>
             dispatch({
               type: "SET_URL",
               payload: `https://hn.algolia.com/api/v1/search?query=${state.query}`,
             })
           }
-          disabled={state.loading}
         >
           Search
         </button>
       </div>
-      <div className="my-3 flex gap-4 flex-wrap">
-        {state.loading && (
-          <div className="loading w-8 h-8 rounded-full border-4 border-r-4 border-blue-400 border-r-transparent animate-spin mx-auto my-10"></div>
-        )}
+
+      {state.loading && (
+        <div className="loading w-8 h-8 rounded-full border-4 border-r-4 border-blue-400 border-r-transparent animate-spin mx-auto my-10"></div>
+      )}
+      {!state.loading && state.errorMsg && (
+        <p className="text-red-500">{state.errorMsg}</p>
+      )}
+
+      <div className="mt-5 mb-5 flex flex-wrap gap-5">
         {!state.loading &&
           state.hits.length > 0 &&
-          state.hits.map((item) => (
-            <h3 className="bg-gray-300 p-4 rounded-md" key={item.title}>
+          state.hits.map((item, index) => (
+            <h3 className="p-2 bg-gray-200 rounded-md" key={index}>
               {item.title}
             </h3>
           ))}
@@ -131,4 +133,4 @@ const HackerNewsReducer = () => {
   );
 };
 
-export default HackerNewsReducer;
+export default HackerNews;
