212d9c54890e3c9419cf181f2d221acc7d1195c9
TranDangKhoi <88824627+TranDangKhoi@users.noreply.github.com>
2022-08-14 17:37:17 +0700

    React Hook Form Validation with Yup and make a loading indicator when submit

--- my-app-2/src/components/Form,Formik,Yup/react-hook-form/ReactHookForm.js
+++ my-app-2/src/components/Form,Formik,Yup/react-hook-form/ReactHookForm.js
@@ -1,15 +1,38 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as Yup from "yup";
+import axios from "axios";
 // using react-hook-form
+const schemaValidation = Yup.object({
+  firstName: Yup.string()
+    .required("Please enter your first name")
+    .max(10, "Your first name must be less than 10 characters"),
+});
 const ReactHookForm = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors },
-  } = useForm();
+
+    formState: { errors, isSubmitting, isValid },
+  } = useForm({
+    resolver: yupResolver(schemaValidation),
+  });
+  console.log("log ~ ReactHookForm ~ isSubmitting", isSubmitting);
   // errors = formState.errors
-  console.log(errors);
-  const onSubmit = (data) => console.log(data);
+  // console.log(errors);
+  const onSubmit = async (data) => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve();
+        console.log(data);
+      }, 4000);
+    });
+    // const response = await axios.get(
+    //   "https://hn.algolia.com/api/v1/search?query=react"
+    // );
+    // return response.data;
+  };
   return (
     <form
       onSubmit={handleSubmit(onSubmit)}
@@ -23,22 +46,14 @@ const ReactHookForm = () => {
           id="firstName"
           className="p-4 rounded-lg border-2 border-gray-200"
           placeholder="Enter your first name"
-          {...register("firstName", {
-            required: true,
-            maxLength: 10,
-            // max: 10, -> dùng cho number
-          })}
+          {...register("firstName")}
         />
-        {errors?.firstName?.type === "required" && (
-          <div className="text-red-500 text-md">
-            Please enter your first name
-          </div>
+        {errors?.firstName && (
+          <div className="text-red-500 text-md">{errors.firstName.message}</div>
         )}
-        {errors?.firstName?.type === "maxLength" && (
-          <div className="text-red-500 text-md">
-            Your first name must be less than 10 characters
-          </div>
-        )}
+        {/* {errors?.firstName?.type === "max" && (
+          <div className="text-red-500 text-md">{errors.firstName.message}</div>
+        )} */}
       </div>
       <div className="flex flex-col gap-2">
         <label htmlFor="lastName">Last Name</label>
@@ -64,7 +79,11 @@ const ReactHookForm = () => {
         type="submit"
         className="w-full bg-blue-500 text-white text-lg rounded-lg p-5"
       >
-        Submit
+        {isSubmitting ? (
+          <div className="mx-auto w-6 h-6 border-2 border-white border-t-2 border-t-transparent rounded-full animate-spin"></div>
+        ) : (
+          "Submit"
+        )}
       </button>
     </form>
   );
