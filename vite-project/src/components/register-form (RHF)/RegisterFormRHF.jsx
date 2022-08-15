8b5b5a9aca48f98819565eb439c8123e5237d932
TranDangKhoi <88824627+TranDangKhoi@users.noreply.github.com>
2022-08-15 15:05:05 +0700

    Finish Custom Input with React Hook Form

--- vite-project/src/components/register-form (RHF)/RegisterFormRHF.jsx
+++ vite-project/src/components/register-form (RHF)/RegisterFormRHF.jsx
@@ -1,22 +1,37 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import InputHook from "../input/InputHook";
 
 const RegisterFormRHF = () => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmitHandler = (values) => {
+    console.log(values);
+  };
   return (
-    <div className="max-w-[300px] mx-auto p-10 my-10">
+    <form
+      onSubmit={handleSubmit(onSubmitHandler)}
+      className="max-w-[300px] mx-auto p-10 my-10"
+    >
       <div className="flex flex-col gap-3">
         <label className="cursor-pointer" htmlFor="username">
           Username
         </label>
-        <input
-          type="text"
-          id="username"
+        <InputHook
           name="username"
-          className="p-4 border-gray-200 border-2 rounded-lg bg-white outline-none transition-all focus:border-blue-400"
           placeholder="Enter your username"
-        />
+          id="username"
+          control={control}
+        ></InputHook>
         <p className="text-red-500 text-sm">Please enter your username</p>
       </div>
-    </div>
+      <button className="w-full p-5 bg-blue-500 text-white rounded-lg mt-5 font-normal">
+        Submit
+      </button>
+    </form>
   );
 };
 
