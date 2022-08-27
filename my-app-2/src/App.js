9b030e13c6942e0f7f122fc08c0e1b2dc72e0b81
TranDangKhoi <88824627+TranDangKhoi@users.noreply.github.com>
2022-08-27 17:51:54 +0700

    Clean code, seperate countProvider and useCount to a individual folder

--- my-app-2/src/App.js
+++ my-app-2/src/App.js
@@ -1,28 +1,11 @@
 import React from "react";
-import { useContext } from "react";
-import { useState } from "react";
-import { createContext } from "react";
 import "./App.css";
+import { CountProvider, useCount } from "./contexts/CountContext";
 // Context
 // VD: Trong App(status: false) có Header, trong Header lại có Menu -> User -> Profile
 // Nếu ta dùng props để truyền giá trị status kia qua từng component như vậy thì sẽ gây ra 1 tình trạng đó chính là Props Drilling
 // Giờ phải dùng Context để truyền giá trị status: false vào profile luôn
-const CountContext = createContext();
-function useCount() {
-  const context = useContext(CountContext);
-  if (typeof context === "undefined") {
-    throw new Error("useCount must be used within a CountProvider");
-  }
-  return context;
-}
 
-function CountProvider(props) {
-  const [count, setCount] = useState(0);
-  const value = [count, setCount];
-  return (
-    <CountContext.Provider value={value} {...props}></CountContext.Provider>
-  );
-}
 function CountDisplay() {
   const [count] = useCount();
   return <div>The count is: {count}</div>;
