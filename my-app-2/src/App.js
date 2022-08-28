c5bccf729c041629cce9831622296f846aa1fa1b
TranDangKhoi <88824627+TranDangKhoi@users.noreply.github.com>
2022-08-28 21:29:05 +0700

    Basic React Router Dom and prepare for next excersise

--- my-app-2/src/App.js
+++ my-app-2/src/App.js
@@ -1,51 +1,35 @@
 import React, { Fragment } from "react";
 import "./App.css";
-import HeaderMain2 from "./components/ContextReact/HeaderMain2";
-import PhotoCartList2 from "./components/Gallery/PhotoCartList2";
-import PhotoList2 from "./components/Gallery/PhotoList2";
-import { AuthProvider2 } from "./contexts/auth-context2";
-import { GalleryProvider2 } from "./contexts/gallery-context2";
+import { Routes, Route } from "react-router-dom";
 
-// Context
-// VD: Trong App(status: false) có Header, trong Header lại có Menu -> User -> Profile
-// Nếu ta dùng props để truyền giá trị status kia qua từng component như vậy thì sẽ gây ra 1 tình trạng đó chính là Props Drilling
-// Giờ phải dùng Context để truyền giá trị status: false vào profile luôn
-
-// function CountDisplay() {
-//   const [count] = useCount();
-//   return <div>The count is: {count}</div>;
-// }
-
-// function Counter() {
-//   const [, setCount] = useCount();
-//   const increment = () => setCount((c) => c + 1);
-//   return (
-//     <button
-//       className="p-4 text-white bg-blue-500 rounded-lg"
-//       onClick={increment}
-//     >
-//       Increment count
-//     </button>
-//   );
-// }
-
+// Link Active Outlet
 function App() {
   return (
-    <Fragment>
-      {/* <div>
-        <CountProvider>
-          <CountDisplay></CountDisplay>
-          <Counter></Counter>
-        </CountProvider>
-      </div> */}
-      <AuthProvider2>
-        <GalleryProvider2>
-          <HeaderMain2></HeaderMain2>
-          <PhotoList2></PhotoList2>
-          <PhotoCartList2></PhotoCartList2>
-        </GalleryProvider2>
-      </AuthProvider2>
-    </Fragment>
+    <div>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi
+              similique ad, consectetur recusandae eaque, corrupti, obcaecati
+              illum sequi delectus eius alias cupiditate fugit laborum nostrum
+              minima adipisci quos voluptatem odio.
+            </div>
+          }
+        ></Route>
+        <Route path="/about" element={<div>This is about page</div>}></Route>
+        <Route
+          path="/movie"
+          element={<div>This is a movie details</div>}
+        ></Route>
+        {/* slug or id (:movieId, :page, etc) */}
+        <Route
+          path="movie/:movieId"
+          element={<div>This is movie's detail of movieId</div>}
+        ></Route>
+      </Routes>
+    </div>
   );
 }
 
