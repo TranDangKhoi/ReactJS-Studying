d5acb7ac2075e1c42b086d6bd5147f5f6da4d631
TranDangKhoi <88824627+TranDangKhoi@users.noreply.github.com>
2022-08-28 19:23:12 +0700

    Finish all basic function (practice context)

--- my-app-2/src/App.js
+++ my-app-2/src/App.js
@@ -1,10 +1,11 @@
 import React, { Fragment } from "react";
 import "./App.css";
-import HeaderMain from "./components/ContextReact/HeaderMain";
-import PhotoCartList from "./components/Gallery/PhotoCartList";
-import PhotoList from "./components/Gallery/PhotoList";
-import { AuthProvider } from "./contexts/authContext";
-import { GalleryProvider } from "./contexts/galleryContext";
+import HeaderMain2 from "./components/ContextReact/HeaderMain2";
+import PhotoCartList2 from "./components/Gallery/PhotoCartList2";
+import PhotoList2 from "./components/Gallery/PhotoList2";
+import { AuthProvider2 } from "./contexts/auth-context2";
+import { GalleryProvider2 } from "./contexts/gallery-context2";
+
 // Context
 // VD: Trong App(status: false) có Header, trong Header lại có Menu -> User -> Profile
 // Nếu ta dùng props để truyền giá trị status kia qua từng component như vậy thì sẽ gây ra 1 tình trạng đó chính là Props Drilling
@@ -37,13 +38,13 @@ function App() {
           <Counter></Counter>
         </CountProvider>
       </div> */}
-      <AuthProvider>
-        <GalleryProvider>
-          <HeaderMain></HeaderMain>
-          <PhotoList></PhotoList>
-          <PhotoCartList></PhotoCartList>
-        </GalleryProvider>
-      </AuthProvider>
+      <AuthProvider2>
+        <GalleryProvider2>
+          <HeaderMain2></HeaderMain2>
+          <PhotoList2></PhotoList2>
+          <PhotoCartList2></PhotoCartList2>
+        </GalleryProvider2>
+      </AuthProvider2>
     </Fragment>
   );
 }
---
+++ my-app-2/src/contexts/gallery-context2.js
@@ -0,0 +1,94 @@
+import { useState, useContext, createContext } from "react";
+const fakeData = [
+  {
+    id: 1,
+    url: "https://images.unsplash.com/photo-1661497118888-98ab158b66d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    isLiked: false,
+  },
+  {
+    id: 2,
+    url: "https://images.unsplash.com/photo-1661598224377-a6d78346d06c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    isLiked: false,
+  },
+  {
+    id: 3,
+    url: "https://images.unsplash.com/photo-1657214059189-6bace4ad1ab8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80",
+    isLiked: true,
+  },
+  {
+    id: 4,
+    url: "https://images.unsplash.com/photo-1661592999164-00b78d12220a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    isLiked: false,
+  },
+  {
+    id: 5,
+    url: "https://images.unsplash.com/photo-1661559858105-57d19d3f4f57?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    isLiked: false,
+  },
+  {
+    id: 6,
+    url: "https://images.unsplash.com/photo-1661347332466-9b6897c93a2c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    isLiked: true,
+  },
+];
+const GalleryContext2 = createContext();
+
+function GalleryProvider2(props) {
+  const [photos, setPhotos] = useState(fakeData);
+  const [cartItems, setCartItems] = useState([]);
+  const [likedList, setLikedList] = useState([]);
+  function likeToggle(photoId) {
+    const updatedGallery = photos.map((photo) => {
+      if (photo.id === photoId) {
+        return { ...photo, isLiked: !photo.isLiked };
+      }
+      return photo;
+    });
+    setPhotos(updatedGallery);
+  }
+
+  function addToCart(newItem) {
+    setCartItems((addedItems) => {
+      const isExisted = addedItems.some((item) => item.id === newItem.id);
+      if (isExisted) {
+        alert("This photo is already existed in your cart");
+        return [...addedItems];
+      }
+      return [...addedItems, newItem];
+    });
+  }
+
+  function deleteFromCart(photoId) {
+    setCartItems((addedItems) =>
+      addedItems.filter((item) => item.id !== photoId)
+    );
+  }
+
+  const value = {
+    photos,
+    cartItems,
+    likedList,
+    setPhotos,
+    setCartItems,
+    setLikedList,
+    likeToggle,
+    addToCart,
+    deleteFromCart,
+  };
+  return (
+    <GalleryContext2.Provider
+      value={value}
+      {...props}
+    ></GalleryContext2.Provider>
+  );
+}
+
+function useGallery2() {
+  const context = useContext(GalleryContext2);
+  if (typeof context === "undefined") {
+    throw new Error("useGallery2 must be used inside GalleryProvider2");
+  }
+  return context;
+}
+
+export { useGallery2, GalleryProvider2 };
