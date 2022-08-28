5711e9bbe73ef3b1009d3ddbdc7ca940740c7e41
TranDangKhoi <88824627+TranDangKhoi@users.noreply.github.com>
2022-08-28 21:49:01 +0700

    Create link to another pages

--- my-app-2/src/App.js
+++ my-app-2/src/App.js
@@ -1,33 +1,19 @@
 import React, { Fragment } from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import Nav from "./components/Router In React/Nav";
+import BlogPage from "./components/Router In React/BlogPage";
+import ProfilePage from "./components/Router In React/ProfilePage";
 
 // Link Active Outlet
 function App() {
   return (
     <div>
+      <Nav></Nav>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi
-              similique ad, consectetur recusandae eaque, corrupti, obcaecati
-              illum sequi delectus eius alias cupiditate fugit laborum nostrum
-              minima adipisci quos voluptatem odio.
-            </div>
-          }
-        ></Route>
-        <Route path="/about" element={<div>This is about page</div>}></Route>
-        <Route
-          path="/movie"
-          element={<div>This is a movie details</div>}
-        ></Route>
-        {/* slug or id (:movieId, :page, etc) */}
-        <Route
-          path="movie/:movieId"
-          element={<div>This is movie's detail of movieId</div>}
-        ></Route>
+        <Route path="/" element={<>Homepage</>}></Route>
+        <Route path="/blog" element={<BlogPage></BlogPage>}></Route>
+        <Route path="/profile" element={<ProfilePage></ProfilePage>}></Route>
       </Routes>
     </div>
   );
---
+++ my-app-2/src/components/Router In React/Nav.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+const Nav = () => {
+  return (
+    <div className="flex items-center justify-center p-5 bg-white shadow-md gap-x-5">
+      <NavLink
+        to={"/"}
+        className={({ isActive }) =>
+          "text-black" + (isActive ? " text-green-400" : "")
+        }
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to={"/blog"}
+        className={({ isActive }) =>
+          "text-black" + (isActive ? " text-green-400" : "")
+        }
+      >
+        Blog
+      </NavLink>
+      <NavLink
+        to={"/profile"}
+        className={({ isActive }) =>
+          "text-black" + (isActive ? " text-green-400" : "")
+        }
+      >
+        Profile
+      </NavLink>
+    </div>
+  );
+};
+
+export default Nav;
