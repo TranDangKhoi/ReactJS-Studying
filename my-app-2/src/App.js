1defdb6fff55ee991a0ee355a069f0b7e9816324
TranDangKhoi <88824627+TranDangKhoi@users.noreply.github.com>
2022-09-26 19:11:36 +0700

    :+1:: New excersise to learn about props collection

--- my-app-2/src/App.js
+++ my-app-2/src/App.js
@@ -1,31 +1,26 @@
 import React from "react";
+import { useState } from "react";
 import "./App.css";
 import Accordion from "./components/advanced-react/react-composition/Accordion";
 import Editable from "./components/advanced-react/react-composition/Editable";
 import HandleValuePropsRender from "./components/advanced-react/render-props/HandleValuePropsRender";
-// import Title from "./components/advanced-react/render-props/Title";
-// render props
-// HOCS = Higher-Order Components
+import Switch from "./components/switch/Switch";
+
+function useToggle() {
+  const [on, setOn] = useState(false);
+  const handleToggle = () => {
+    setOn(!on);
+  };
+  return { on, handleToggle };
+}
+
 function App() {
+  const { on, handleToggle } = useToggle();
   return (
-    <div className="p-10 w-full max-w-[600px] mx-auto">
-      {/* <Title>{() => <h1>Hello 😀</h1>}</Title> */}
-      {/* <HandleValuePropsRender></HandleValuePropsRender> */}
-      <Accordion header="Can i change my plan later?">
-        <div>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non
-          necessitatibus sunt ab commodi ad quia at voluptatum et eligendi porro
-          sit optio vero exercitationem nam alias officiis iste, quis odio?
-        </div>
-      </Accordion>
-      <Accordion header="Can I use React in Angular?">
-        <div>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non
-          necessitatibus sunt ab commodi ad quia at voluptatum et eligendi porro
-          sit optio vero exercitationem nam alias officiis iste, quis odio?
-        </div>
-      </Accordion>
-      {/* <Editable></Editable> */}
+    <div>
+      <Switch on={on} onClick={handleToggle}></Switch>
+      <br />
+      <button aria-label="custom-button">{on ? "on" : "off"}</button>
     </div>
   );
 }
